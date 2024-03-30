0e4be961962831ef2cab0d8a349965d0655b5230
dannycho1530 <46494084+dannycho1530@users.noreply.github.com>
2024-03-30 16:33:34 +0900

    Time: 65 ms (40.33%), Space: 17 MB (42.28%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,28 +1,44 @@
 class Solution:
-    def solveNQueens(self, n):
-        def Queens(row, board):
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        # 열, 대각선 확인하는 func
+        def is_valid(board, row, col):
+            #열
+            for i in range(row):
+                if board[i][col] == 'Q':
+                    return False
+            
+            #오른쪽 대각선
+            i, j = row-1, col+1
+            while i>=0 and j<n:
+                if board[i][j] == 'Q':
+                    return False
+                i-=1
+                j+=1
+            
+            #왼쪽 대각선
+            i, j = row-1, col-1
+            while i>=0 and j>=0:
+                if board[i][j] == 'Q':
+                    return False
+                i-=1
+                j-=1
+            
+            return True
+        
+        def solution(row):
+            # 줄이 최종에 닿았을때,
             if row == n:
-                res.append(["".join(r) for r in board])
+                answer.append(["".join(row) for row in board])
                 return
-
+            
             for col in range(n):
-                if (
-                    col not in vertical
-                    and (row - col) not in diagonalleft
-                    and (row + col) not in diagonalright
-                ):
-                    board[row][col] = "Q"
-                    vertical.add(col)
-                    diagonalleft.add(row - col)
-                    diagonalright.add(row + col)
-                    Queens(row + 1, board)
-                    board[row][col] = "."
-                    vertical.remove(col)
-                    diagonalleft.remove(row - col)
-                    diagonalright.remove(row + col)
-
-        res = []
-        board = [["." for _ in range(n)] for _ in range(n)]
-        vertical, diagonalleft, diagonalright = set(), set(), set()
-        Queens(0, board)
-        return res
+                if is_valid(board, row, col):
+                    board[row][col] = 'Q'
+                    solution(row+1)
+                    board[row][col] = '.'
+        
+        answer = []
+        board = [['.' for _ in range(n)] for _ in range(n)]
+        solution(0)
+        
+        return answer
